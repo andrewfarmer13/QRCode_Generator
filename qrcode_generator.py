b3fcce07e3da020481dd2b54e03d14a2f1e91230
andrewfarmer13 <andrewfarmer13@gmail.com>
2022-10-23 23:58:18 -0500

    updated source

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -6,8 +6,9 @@
 import qrcode
 
 url = input("Enter the Url that you wish to convert to a qr code: ")
+name = input("What do you want to name the qr_code: ")
 
 qr = qrcode.make(url)
 
-qr.save("my_qr.png")
+qr.save(name + ".png")
 
